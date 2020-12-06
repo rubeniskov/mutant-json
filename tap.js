e19ba487fcea822445a16dc09e70ddfc19b5ba07
rubeniskov <me@rubeniskov.com>
2020-12-07 00:48:22 +0100

    fix: tap add null condition

--- tap.js
+++ tap.js
@@ -2,6 +2,6 @@ const {
   JSONPATCH_SEP,
 } = require('./constants');
 
-const tap = (obj, path, defaultValue) => path.substring(1).split(JSONPATCH_SEP).reduce((prev, chunk) => typeof prev === 'object' && prev[chunk] !== undefined ? prev[chunk] : defaultValue, obj);
+const tap = (obj, path, defaultValue) => path.substring(1).split(JSONPATCH_SEP).reduce((prev, chunk) => typeof prev === 'object' && prev !== null && prev[chunk] !== undefined ? prev[chunk] : defaultValue, obj);
 
 module.exports = tap;
